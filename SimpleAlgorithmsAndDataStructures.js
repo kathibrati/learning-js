39a1013076c81e83e53fe833c5757bb719946fec
kathibrati <cskathi@web.de>
2021-06-16 21:03:47 +0200

    Beginning Basics

---
+++ SimpleAlgorithmsAndDataStructures.js
@@ -0,0 +1,13 @@
+//Algorithms and Data Structures
+
+
+
+//boolean and if-statement
+function trueOrFalse(wasThatTrue) {
+
+  if (wasThatTrue == true) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  }
